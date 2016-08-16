23cf72b7f418ab7c7fcce81aed6cc81bcd026e17
unknown <n1507@CL37G206.sit.nyp.edu.sg>
2016-08-16 10:16:35 +0800

    Quiz files created

---
+++ SP1Framework/quiz.h
@@ -0,0 +1,17 @@
+#ifndef _QUIZ_H
+#define _QUIZ_H
+
+#include <string>
+
+using namespace std;
+
+struct QuizQuestion
+{
+	string Question;
+	string Answer;
+};
+
+struct QuizQuestion stQuestion[30];
+struct QuizAnswer stAnswer[30];
+
+#endif
