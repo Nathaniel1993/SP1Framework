7e373ac3787e905534b3cce4a4618011115fe91c
unknown <n1507@CL37G206.sit.nyp.edu.sg>
2016-08-16 13:02:48 +0800

    All 30 questions to be asked and answers to them completed

--- SP1Framework/quiz.h
+++ SP1Framework/quiz.h
@@ -2,15 +2,17 @@
 #define _QUIZ_H
 
 #include <string>
+#include <iostream>
 
 using namespace std;
 
 struct QuizQuestion
 {
-	string Question;
-	string Answer;
+	string question;
+	string answer1;
+	string answer2;
 };
 
 struct QuizQuestion stQuestion[30];
-
+void Quiz(void);
 #endif
