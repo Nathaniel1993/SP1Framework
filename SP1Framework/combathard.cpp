1ca05482b33b7a74b3d909fa91510e40320b28cc
unknown <n1507@CL37G206.sit.nyp.edu.sg>
2016-08-24 17:31:26 +0800

    combat system fully complete (yet to be tested) - Marcus

--- SP1Framework/combathard.cpp
+++ SP1Framework/combathard.cpp
@@ -1,183 +1,183 @@
 #include"combat.h"
 #include <fstream>
 
-//extern bool g_abKeyPressed[K_COUNT];
-//extern EGAMESTATES g_eGameState;
-//extern Console g_Console;
-//extern char MapSize[80][31];
-//extern int Lifecounter;
-//
-//void HardQuestion1()
-//{
-//	if (g_abKeyPressed[K_ONE])
-//	{
-//		g_eGameState = S_GAME;
-//	}
-//	else
-//	{
-//		Lifecounter++;
-//	}
-//}
-//
-//void HardQuestion2()
-//{
-//	if (g_abKeyPressed[K_TWO])
-//	{
-//		g_eGameState = S_GAME;
-//	}
-//	else
-//	{
-//		Lifecounter++;
-//	}
-//}
-//
-//void HardQuestion3()
-//{
-//	if (g_abKeyPressed[K_THREE])
-//	{
-//		g_eGameState = S_GAME;
-//	}
-//	else
-//	{
-//		Lifecounter++;
-//	}
-//}
-//
-//void HardQuestion4()
-//{
-//	if (g_abKeyPressed[K_FOUR])
-//	{
-//		g_eGameState = S_GAME;
-//	}
-//	else
-//	{
-//		Lifecounter++;
-//	}
-//}
-//
-//void HardQuestion5()
-//{
-//	if (g_abKeyPressed[K_FIVE])
-//	{
-//		g_eGameState = S_GAME;
-//	}
-//	else
-//	{
-//		Lifecounter++;
-//	}
-//}
-//
-//void renderHardQuestion1()
-//{
-//	COORD l;
-//	l.X = 50;
-//	l.Y = 7;
-//	g_Console.writeToBuffer(l, "Who wrote the 'Sword Art Online Series'?", 0x0A);
-//	l.Y = 28;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "1: Reki Kawahara", 0x0B);
-//	l.Y = 28;
-//	l.X = 50;
-//	g_Console.writeToBuffer(l, "2: Sui Ishida", 0x0B);
-//	l.Y = 29;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "3: Yuto Tsukuda", 0x0B);
-//	l.Y = 29;
-//	l.X = 50;
-//	g_Console.writeToBuffer(l, "4: Eiichiro Oda", 0x0B);
-//	l.Y = 30;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "5: Tite Kubo", 0x0B);
-//}
-//
-//void renderHardQuestion2()
-//{
-//	COORD l;
-//	l.X = 50;
-//	l.Y = 7;
-//	g_Console.writeToBuffer(l, "In Shakesphere's novel, which country is Hamlet the prince of?", 0x0A);
-//	l.Y = 28;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "1: Spain", 0x0B);
-//	l.Y = 28;
-//	l.X = 50;
-//	g_Console.writeToBuffer(l, "2: Denmark", 0x0B);
-//	l.Y = 29;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "3: France", 0x0B);
-//	l.Y = 29;
-//	l.X = 50;
-//	g_Console.writeToBuffer(l, "4: England", 0x0B);
-//	l.Y = 30;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "5: Germany", 0x0B);
-//}
-//
-//void renderHardQuestion3()
-//{
-//	COORD l;
-//	l.X = 50;
-//	l.Y = 7;
-//	g_Console.writeToBuffer(l, "What is the game that inspired making of MOBA games?", 0x0A);
-//	l.Y = 28;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "1: League of Legends", 0x0B);
-//	l.Y = 28;
-//	l.X = 50;
-//	g_Console.writeToBuffer(l, "2: Dota 2", 0x0B);
-//	l.Y = 29;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "3: Warcraft 3", 0x0B);
-//	l.Y = 29;
-//	l.X = 50;
-//	g_Console.writeToBuffer(l, "4: Starcraft", 0x0B);
-//	l.Y = 30;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "5: Dota", 0x0B);
-//}
-//
-//void renderHardQuestion4()
-//{
-//	COORD l;
-//	l.X = 50;
-//	l.Y = 7;
-//	g_Console.writeToBuffer(l, "How are Pokemon game versions named after?", 0x0A);
-//	l.Y = 28;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "1: Main antagonist of the game", 0x0B);
-//	l.Y = 28;
-//	l.X = 50;
-//	g_Console.writeToBuffer(l, "2: Randomly think of a name", 0x0B);
-//	l.Y = 29;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "3: The name of the region", 0x0B);
-//	l.Y = 29;
-//	l.X = 50;
-//	g_Console.writeToBuffer(l, "4: Main protagonist of the region in the manga", 0x0B);
-//	l.Y = 30;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "5: The anime title of the region", 0x0B);
-//}
-//
-//void renderHardQuestion5()
-//{
-//	COORD l;
-//	l.X = 50;
-//	l.Y = 7;
-//	g_Console.writeToBuffer(l, "Who came out with the formula 'e = mc^2'?", 0x0A);
-//	l.Y = 28;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "1: Amelia Earhart", 0x0B);
-//	l.Y = 28;
-//	l.X = 50;
-//	g_Console.writeToBuffer(l, "2: Euclid", 0x0B);
-//	l.Y = 29;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "3: Thomas Edison", 0x0B);
-//	l.Y = 29;
-//	l.X = 50;
-//	g_Console.writeToBuffer(l, "4: Isaac Newton", 0x0B);
-//	l.Y = 30;
-//	l.X = 3;
-//	g_Console.writeToBuffer(l, "5: Albert Einstein", 0x0B);
-//}
+extern bool g_abKeyPressed[K_COUNT];
+extern EGAMESTATES g_eGameState;
+extern Console g_Console;
+extern char MapSize[80][31];
+extern int Lifecounter;
+
+void HardQuestion1()
+{
+	if (g_abKeyPressed[K_ONE])
+	{
+		g_eGameState = S_GAME;
+	}
+	else
+	{
+		Lifecounter++;
+	}
+}
+
+void HardQuestion2()
+{
+	if (g_abKeyPressed[K_TWO])
+	{
+		g_eGameState = S_GAME;
+	}
+	else
+	{
+		Lifecounter++;
+	}
+}
+
+void HardQuestion3()
+{
+	if (g_abKeyPressed[K_THREE])
+	{
+		g_eGameState = S_GAME;
+	}
+	else
+	{
+		Lifecounter++;
+	}
+}
+
+void HardQuestion4()
+{
+	if (g_abKeyPressed[K_FOUR])
+	{
+		g_eGameState = S_GAME;
+	}
+	else
+	{
+		Lifecounter++;
+	}
+}
+
+void HardQuestion5()
+{
+	if (g_abKeyPressed[K_FIVE])
+	{
+		g_eGameState = S_GAME;
+	}
+	else
+	{
+		Lifecounter++;
+	}
+}
+
+void renderHardQuestion1()
+{
+	COORD l;
+	l.X = 50;
+	l.Y = 7;
+	g_Console.writeToBuffer(l, "Who wrote the 'Sword Art Online Series'?", 0x0A);
+	l.Y = 28;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "1: Reki Kawahara", 0x0B);
+	l.Y = 28;
+	l.X = 50;
+	g_Console.writeToBuffer(l, "2: Sui Ishida", 0x0B);
+	l.Y = 29;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "3: Yuto Tsukuda", 0x0B);
+	l.Y = 29;
+	l.X = 50;
+	g_Console.writeToBuffer(l, "4: Eiichiro Oda", 0x0B);
+	l.Y = 30;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "5: Tite Kubo", 0x0B);
+}
+
+void renderHardQuestion2()
+{
+	COORD l;
+	l.X = 50;
+	l.Y = 7;
+	g_Console.writeToBuffer(l, "In Shakesphere's novel, which country is Hamlet the prince of?", 0x0A);
+	l.Y = 28;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "1: Spain", 0x0B);
+	l.Y = 28;
+	l.X = 50;
+	g_Console.writeToBuffer(l, "2: Denmark", 0x0B);
+	l.Y = 29;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "3: France", 0x0B);
+	l.Y = 29;
+	l.X = 50;
+	g_Console.writeToBuffer(l, "4: England", 0x0B);
+	l.Y = 30;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "5: Germany", 0x0B);
+}
+
+void renderHardQuestion3()
+{
+	COORD l;
+	l.X = 50;
+	l.Y = 7;
+	g_Console.writeToBuffer(l, "What is the game that inspired making of MOBA games?", 0x0A);
+	l.Y = 28;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "1: League of Legends", 0x0B);
+	l.Y = 28;
+	l.X = 50;
+	g_Console.writeToBuffer(l, "2: Dota 2", 0x0B);
+	l.Y = 29;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "3: Warcraft 3", 0x0B);
+	l.Y = 29;
+	l.X = 50;
+	g_Console.writeToBuffer(l, "4: Starcraft", 0x0B);
+	l.Y = 30;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "5: Dota", 0x0B);
+}
+
+void renderHardQuestion4()
+{
+	COORD l;
+	l.X = 50;
+	l.Y = 7;
+	g_Console.writeToBuffer(l, "How are Pokemon game versions named after?", 0x0A);
+	l.Y = 28;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "1: Main antagonist of the game", 0x0B);
+	l.Y = 28;
+	l.X = 50;
+	g_Console.writeToBuffer(l, "2: Randomly think of a name", 0x0B);
+	l.Y = 29;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "3: The name of the region", 0x0B);
+	l.Y = 29;
+	l.X = 50;
+	g_Console.writeToBuffer(l, "4: Main protagonist of the region in the manga", 0x0B);
+	l.Y = 30;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "5: The anime title of the region", 0x0B);
+}
+
+void renderHardQuestion5()
+{
+	COORD l;
+	l.X = 50;
+	l.Y = 7;
+	g_Console.writeToBuffer(l, "Who came out with the formula 'e = mc^2'?", 0x0A);
+	l.Y = 28;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "1: Amelia Earhart", 0x0B);
+	l.Y = 28;
+	l.X = 50;
+	g_Console.writeToBuffer(l, "2: Euclid", 0x0B);
+	l.Y = 29;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "3: Thomas Edison", 0x0B);
+	l.Y = 29;
+	l.X = 50;
+	g_Console.writeToBuffer(l, "4: Isaac Newton", 0x0B);
+	l.Y = 30;
+	l.X = 3;
+	g_Console.writeToBuffer(l, "5: Albert Einstein", 0x0B);
+}
--- SP1Framework/combatmedium.cpp
+++ SP1Framework/combatmedium.cpp
@@ -1,189 +1,189 @@
 #include"combat.h"
 #include <fstream>
 
-//extern bool g_abKeyPressed[K_COUNT];
-//extern EGAMESTATES g_eGameState;
-//extern Console g_Console;
-//extern char MapSize[80][31];
-//extern int Lifecounter;
-//
-//void MediumQuestion1()
-//{
-//	if (g_abKeyPressed[K_THREE])
-//	{
-//		g_eGameState = S_GAME;
-//	}
-//	else
-//	{
-//		Lifecounter++;
-//	}
-//}
-//
-//void MediumQuestion2()
-//{
-//	if (g_abKeyPressed[])
-//	{
-//		g_eGameState = S_GAME;
-//	}
-//	else
-//	{
-//		Lifecounter++;
-//	}
-//}
-//
-//void MediumQuestion3()
-//{
-//	if (g_abKeyPressed[])
-//	{
-//		g_eGameState = S_GAME;
-//	}
-//	else
-//	{
-//		Lifecounter++;
-//	}
-//}
-//
-//void MediumQuestion4()
-//{
-//	if (g_abKeyPressed[])
-//	{
-//		g_eGameState = S_GAME;
-//	}
-//	else
-//	{
-//		Lifecounter++;
-//	}
-//}
-//
-//void MediumQuestion5()
-//{
-//	if (g_abKeyPressed[])
-//	{
-//		g_eGameState = S_GAME;
-//	}
-//	else
-//	{
-//		Lifecounter++;
-//	}
-//}
-//
-//void renderMediumQuestion1()
-//{
-//	COORD l;
-//	l.X = 2;
-//	l.Y = 24;
-//	g_Console.writeToBuffer(l, "Johnny’s mother had three children. The first child was named April.", 0x0A);
-//  l.X = 2;
-//	l.Y = 25;
-//  g_Console.writeToBuffer(l, "The second child was named May. What was the third child’s name?",0x0A);
-//	l.X = 2;
-//	l.Y = 26;
-//	g_Console.writeToBuffer(l, "1: June", 0x0B);
-//	l.X = 50;
-//	l.Y = 26;
-//	g_Console.writeToBuffer(l, "2: Jan", 0x0B);
-//	l.X = 2;
-//	l.Y = 27;
-//	g_Console.writeToBuffer(l, "3: Johnny", 0x0B); //Correct
-//	l.X = 50;
-//	l.Y = 27;
-//	g_Console.writeToBuffer(l, "4: Jake", 0x0B);
-//	l.X = 2;
-//	l.Y = 28;
-//	g_Console.writeToBuffer(l, "5: Janny", 0x0B);
-//}
-//
-//void renderMediumQuestion2()
-//{
-//	COORD l;
-//	l.X = 2;
-//	l.Y = 24;
-//	g_Console.writeToBuffer(l, "Before Mt. Everest was discovered, what was the highest mountain in the world?", 0x0A);
-//	l.X = 2;
-//	l.Y = 26;
-//	g_Console.writeToBuffer(l, "1: Mt. Moon", 0x0B);
-//	l.X = 50;
-//	l.Y = 26;
-//	g_Console.writeToBuffer(l, "2: Mt Fuji", 0x0B);
-//	l.X = 2;
-//	l.Y = 27;
-//	g_Console.writeToBuffer(l, "3: Mt. Kilimanjaro", 0x0B);
-//	l.X = 50;
-//	l.Y = 27;
-//	g_Console.writeToBuffer(l, "4: Mt. Everest", 0x0B);
-//	l.X = 2;
-//	l.Y = 28;
-//	g_Console.writeToBuffer(l, "5: The Black Mountain", 0x0B);
-//}
-//
-//void renderMediumQuestion3()
-//{
-//	COORD l;
-//	l.X = 2;
-//	l.Y = 24;
-//	g_Console.writeToBuffer(l, "If there are 6 apples and you take away 4, how many do you have?", 0x0A);
-//	l.X = 2;
-//	l.Y = 26;
-//	g_Console.writeToBuffer(l, "1: 1 apples", 0x0B);
-//	l.X = 50;
-//	l.Y = 26;
-//	g_Console.writeToBuffer(l, "2: 2 apples", 0x0B);
-//	l.X = 2;
-//	l.Y = 27;
-//	g_Console.writeToBuffer(l, "3: 3 apples", 0x0B);
-//	l.X = 50;
-//	l.Y = 27;
-//	g_Console.writeToBuffer(l, "4: 4 apples", 0x0B);
-//	l.X = 2;
-//	l.Y = 28;
-//	g_Console.writeToBuffer(l, "5: None", 0x0B);
-//}
-//
-//void renderMediumQuestion4()
-//{
-//	COORD l;
-//	l.X = 2;
-//	l.Y = 24;
-//	g_Console.writeToBuffer(l, "If you had only one match, and entered a dark room containing an oil lamp,", 0x0A);
-//	l.X = 2;
-//	l.Y = 26;
-//	g_Console.writeToBuffer(l, "some newspaper, and some kindling wood, which would you light first? ", 0x0A);
-//	l.X = 2;
-//	l.Y = 27;
-//	g_Console.writeToBuffer(l, "1: Oil lamp", 0x0B);
-//	l.X = 50;
-//	l.Y = 28;
-//	g_Console.writeToBuffer(l, "2: The Newspaper", 0x0B);
-//	l.X = 2;
-//	l.Y = 28;
-//	g_Console.writeToBuffer(l, "3: The Wood", 0x0B);
-//	l.X = 50;
-//	l.Y = 29;
-//	g_Console.writeToBuffer(l, "4: The Match", 0x0B);
-//	l.X = 2;
-//	l.Y = 29;
-//	g_Console.writeToBuffer(l, "5: The dark room", 0x0B);
-//}
-//
-//void renderMediumQuestion5()
-//{
-//	COORD l;
-//	l.X = 2;
-//	l.Y = 24;
-//	g_Console.writeToBuffer(l, "A little girl kicks a soccer ball. It goes 7 metres and comes back to her. How is this possible?", 0x0A);
-//	l.X = 2;
-//	l.Y = 26;
-//	g_Console.writeToBuffer(l, "1: Gravity", 0x0B);
-//	l.X = 50;
-//	l.Y = 26;
-//	g_Console.writeToBuffer(l, "2: Witch craft", 0x0B);
-//	l.X = 2;
-//	l.Y = 27;
-//	g_Console.writeToBuffer(l, "3: Ball is a boomarang", 0x0B);
-//	l.X = 50;
-//	l.Y = 27;
-//	g_Console.writeToBuffer(l, "4: Impossible", 0x0B);
-//	l.X = 2;
-//	l.Y = 28;
-//	g_Console.writeToBuffer(l, "5: All but 4", 0x0B);
-//}
+extern bool g_abKeyPressed[K_COUNT];
+extern EGAMESTATES g_eGameState;
+extern Console g_Console;
+extern char MapSize[80][31];
+extern int Lifecounter;
+
+void MediumQuestion1()
+{
+	if (g_abKeyPressed[K_THREE])
+	{
+		g_eGameState = S_GAME;
+	}
+	else
+	{
+		Lifecounter++;
+	}
+}
+
+void MediumQuestion2()
+{
+	if (g_abKeyPressed[K_FOUR])
+	{
+		g_eGameState = S_GAME;
+	}
+	else
+	{
+		Lifecounter++;
+	}
+}
+
+void MediumQuestion3()
+{
+	if (g_abKeyPressed[K_FOUR])
+	{
+		g_eGameState = S_GAME;
+	}
+	else
+	{
+		Lifecounter++;
+	}
+}
+
+void MediumQuestion4()
+{
+	if (g_abKeyPressed[K_FOUR])
+	{
+		g_eGameState = S_GAME;
+	}
+	else
+	{
+		Lifecounter++;
+	}
+}
+
+void MediumQuestion5()
+{
+	if (g_abKeyPressed[K_ONE])
+	{
+		g_eGameState = S_GAME;
+	}
+	else
+	{
+		Lifecounter++;
+	}
+}
+
+void renderMediumQuestion1()
+{
+	COORD l;
+	l.X = 2;
+	l.Y = 24;
+	g_Console.writeToBuffer(l, "Johnny’s mother had three children. The first child was named April.", 0x0A);
+  l.X = 2;
+	l.Y = 25;
+  g_Console.writeToBuffer(l, "The second child was named May. What was the third child’s name?",0x0A);
+	l.X = 2;
+	l.Y = 26;
+	g_Console.writeToBuffer(l, "1: June", 0x0B);
+	l.X = 50;
+	l.Y = 26;
+	g_Console.writeToBuffer(l, "2: Jan", 0x0B);
+	l.X = 2;
+	l.Y = 27;
+	g_Console.writeToBuffer(l, "3: Johnny", 0x0B); 
+	l.X = 50;
+	l.Y = 27;
+	g_Console.writeToBuffer(l, "4: Jake", 0x0B);
+	l.X = 2;
+	l.Y = 28;
+	g_Console.writeToBuffer(l, "5: Janny", 0x0B);
+}
+
+void renderMediumQuestion2()
+{
+	COORD l;
+	l.X = 2;
+	l.Y = 24;
+	g_Console.writeToBuffer(l, "Before Mt. Everest was discovered, what was the highest mountain in the world?", 0x0A);
+	l.X = 2;
+	l.Y = 26;
+	g_Console.writeToBuffer(l, "1: Mt. Moon", 0x0B);
+	l.X = 50;
+	l.Y = 26;
+	g_Console.writeToBuffer(l, "2: Mt Fuji", 0x0B);
+	l.X = 2;
+	l.Y = 27;
+	g_Console.writeToBuffer(l, "3: Mt. Kilimanjaro", 0x0B);
+	l.X = 50;
+	l.Y = 27;
+	g_Console.writeToBuffer(l, "4: Mt. Everest", 0x0B);
+	l.X = 2;
+	l.Y = 28;
+	g_Console.writeToBuffer(l, "5: The Black Mountain", 0x0B);
+}
+
+void renderMediumQuestion3()
+{
+	COORD l;
+	l.X = 2;
+	l.Y = 24;
+	g_Console.writeToBuffer(l, "If there are 6 apples and you take away 4, how many do you have?", 0x0A);
+	l.X = 2;
+	l.Y = 26;
+	g_Console.writeToBuffer(l, "1: 1 apples", 0x0B);
+	l.X = 50;
+	l.Y = 26;
+	g_Console.writeToBuffer(l, "2: 2 apples", 0x0B);
+	l.X = 2;
+	l.Y = 27;
+	g_Console.writeToBuffer(l, "3: 3 apples", 0x0B);
+	l.X = 50;
+	l.Y = 27;
+	g_Console.writeToBuffer(l, "4: 4 apples", 0x0B);
+	l.X = 2;
+	l.Y = 28;
+	g_Console.writeToBuffer(l, "5: None", 0x0B);
+}
+
+void renderMediumQuestion4()
+{
+	COORD l;
+	l.X = 2;
+	l.Y = 24;
+	g_Console.writeToBuffer(l, "If you had only one match, and entered a dark room containing an oil lamp,", 0x0A);
+	l.X = 2;
+	l.Y = 25;
+	g_Console.writeToBuffer(l, "some newspaper, and some kindling wood, which would you light first? ", 0x0A);
+	l.X = 2;
+	l.Y = 27;
+	g_Console.writeToBuffer(l, "1: Oil lamp", 0x0B);
+	l.X = 50;
+	l.Y = 28;
+	g_Console.writeToBuffer(l, "2: The Newspaper", 0x0B);
+	l.X = 2;
+	l.Y = 28;
+	g_Console.writeToBuffer(l, "3: The Wood", 0x0B);
+	l.X = 50;
+	l.Y = 29;
+	g_Console.writeToBuffer(l, "4: The Match", 0x0B);
+	l.X = 2;
+	l.Y = 29;
+	g_Console.writeToBuffer(l, "5: The dark room", 0x0B);
+}
+
+void renderMediumQuestion5()
+{
+	COORD l;
+	l.X = 2;
+	l.Y = 24;
+	g_Console.writeToBuffer(l, "A little girl kicks a soccer ball. It goes 7 metres and comes back to her. How is this possible?", 0x0A);
+	l.X = 2;
+	l.Y = 26;
+	g_Console.writeToBuffer(l, "1: Gravity", 0x0B);
+	l.X = 50;
+	l.Y = 26;
+	g_Console.writeToBuffer(l, "2: Witch craft", 0x0B);
+	l.X = 2;
+	l.Y = 27;
+	g_Console.writeToBuffer(l, "3: Ball is a boomarang", 0x0B);
+	l.X = 50;
+	l.Y = 27;
+	g_Console.writeToBuffer(l, "4: Impossible", 0x0B);
+	l.X = 2;
+	l.Y = 28;
+	g_Console.writeToBuffer(l, "5: All but 4", 0x0B);
+}
